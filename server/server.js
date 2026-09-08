2f7279d30f4ec50b8cdd22ca15583a21e89405c8
mishz1995 <120318072+mishz1995@users.noreply.github.com>
2026-09-08 15:10:35 +0300

    Add route to serve QR HTML file

--- server/server.js
+++ server/server.js
@@ -19,6 +19,9 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.get('/qr', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/qr.html'));
+});
 
 const orders = new Map();
 let orderCounter = 1000;
